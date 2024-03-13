fc7fc84137442378a29b94573724e92333f15177
jpmacleure <macleure-@hotmail.com>
2024-03-13 10:05:24 -0300

    add atividade1

---
+++ atividade1/variaveisOperados.js
@@ -0,0 +1,18 @@
+const a = 2;
+const b = 3;
+const d = "2";
+const e = "3";
+
+let c = a + b;
+console.log('O resultado da soma entre ' + a + ' e ' + b + ' é ' + c);
+
+c = a - b;
+console.log(`O resultado da subtração entre ${a} e ${b} é ${c}`);
+
+c = a * b;
+console.log(`O resultado da multiplicação entre ${a} e ${b} é ${c}`);
+
+c = a / b;
+console.log(`O resultado da divisão entre ${a} e ${b} é ${c}`);
+
+console.log(`O resultado da soma entre ${d} e ${e} é ${d + e}`);
