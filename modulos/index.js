90897f93c32638a1ac9a94e51af7e7b81d7e6b5a
jpmacleure <macleure-@hotmail.com>
2024-03-27 11:03:40 -0300

    add modulos

---
+++ modulos/index.js
@@ -0,0 +1,13 @@
+const matriz_util = require('./util/matriz_util');
+const calc = require('./util/calculadora');
+const express = require('express');
+const app = express();
+
+app.get('/ola', function(req, res){
+    res.send('Olá, mundo!');
+});
+
+const PORT = 8080;
+app.listen(PORT, function (){
+    console.log('app rodando na porta ' + PORT);
+});
---
+++ modulos/util/calculadora.js
@@ -0,0 +1,25 @@
+function somar(a, b){
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
+
+const PI = 3.14
+
+module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir,
+    PI
+};
